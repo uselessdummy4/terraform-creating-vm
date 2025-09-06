b013343246a0c676155892b0e5e5ab08244371b4
uselessdummy4 <uselessdummy4@gmail.com>
2025-09-06 20:45:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "ec2_public_ips" {
   description = "Public IPs of all EC2 instances"
   value       = [for i in aws_instance.ubuntu_ec2 : i.public_ip]
 }
-
-output "private_key_file" {
-  description = "Private key saved as host.pem"
-  value       = local_file.private_key_file.filename
-}
