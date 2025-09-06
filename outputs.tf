d274ce2558424e154d54465835e2d01c698629d6
uselessdummy4 <uselessdummy4@gmail.com>
2025-09-06 21:34:38 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "ec2_public_ips" {
-  description = "Public IPs of all EC2 instances"
-  value       = [for i in aws_instance.ubuntu_ec2 : i.public_ip]
+output "ec2_name_ip_map" {
+  description = "Map of EC2 instance Name to Public IP"
+  value = { for idx, inst in aws_instance.ubuntu_ec2 : inst.tags["Name"] => inst.public_ip }
 }
